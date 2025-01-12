eb63b67b63bd97624996dcc7cac334b73dad88f3
Huy Ph <huypmg.16@gmail.com>
2025-01-12 13:17:02 +0700

    deploy

--- BlogVerse-FE/vite.config.mjs
+++ BlogVerse-FE/vite.config.mjs
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  esbuild: {
-    target: 'esnext',
-    platform: 'linux',
-  } 
+    base: './',
 });
